10bca5704ec0bce4b9fcd4073f4f0a9946ca2e76
BT_1N3_31@bktech.local <BT_1N3_31@bktech.local>
2018-09-25 11:40:56 -0400

    Initial commit

---
+++ src/JWuGuess/Main.java
@@ -0,0 +1,19 @@
+package JWuGuess;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        int secretNumber = (int) (10 * Math.random());
+
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("What is the Number?");
+        int num = keyboard.nextInt();
+        if(num)
+    }
+}
