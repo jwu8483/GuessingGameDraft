2ac21baaaff44f7339d66e8f9e2f0aeb6005a66e
jwu8483 <43213089+jwu8483@users.noreply.github.com>
2018-09-26 11:16:23 -0400

    Update Main.java

--- src/JWuGuess/Main.java
+++ src/JWuGuess/Main.java
@@ -7,13 +7,26 @@ public class Main {
     public static void main(String[] args) {
 
 
-        int secretNumber = (int) (10 * Math.random());
+        int secretNumber = (int) (100 * Math.random());
+        int Guess = 0;
 
 
         Scanner keyboard = new Scanner(System.in);
+        do {
 
-        System.out.println("What is the Number?");
-        int num = keyboard.nextInt();
-        if(num)
+            System.out.println("What is the Number?");
+            int num = keyboard.nextInt();
+            if (num == secretNumber) {
+                System.out.println("You are correct");
+                Guess = Guess + 1;
+                System.out.println("It took you " + Guess + " 1" + "Guesses");
+            } else if (num > secretNumber) {
+                System.out.println("You are too high");
+                Guess = Guess + 1;
+            } else {
+                System.out.println("You are too low");
+                Guess = Guess + 1;
+            }
+        }while (Guess != secretNumber);
     }
 }
