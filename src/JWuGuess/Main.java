24191456689474851654d65941fd52e8275bb9f5
jwu8483 <43213089+jwu8483@users.noreply.github.com>
2018-09-27 11:12:12 -0400

    Update Main.java

--- src/JWuGuess/Main.java
+++ src/JWuGuess/Main.java
@@ -1,39 +1,47 @@
-package JWuGuess;
-
-import java.util.Scanner;
+package JwuGuess;
 
 public class Main {
 
     public static void main(String[] args) {
 
 
-        int secretNumber = (int) (100 * Math.random());
-        int Guess = 0;
+                int secretNumber = (int) (100 * Math.random());
+                int Guess = 0;
 
 
-        Scanner keyboard = new Scanner(System.in);
-        do {
+                Scanner keyboard = new Scanner(System.in);
+                do {
 
-            System.out.println("What is the Number?");
-            int num = keyboard.nextInt();
-            if (num == secretNumber) {
-                System.out.println("You are correct");
-                Guess = Guess + 1;
-                System.out.println("It took you " + Guess + " 1" + "Guesses");
-            } else if (num > secretNumber) {
-                System.out.println("You are too high");
-                Guess = Guess + 1;
-            } else {
-                System.out.println("You are too low");
-                Guess = Guess + 1;
+                    System.out.println("What is the Number?");
+                    int num = keyboard.nextInt();
+                    if (num == secretNumber) {
+                        System.out.println("You are correct");
+                        Guess = Guess + 1;
+                        System.out.println("It took you " + Guess + " 1" + "Guesses");
+                    } else if (num > secretNumber) {
+                        System.out.println("You are too high");
+                        Guess = Guess + 1;
+                    } else {
+                        System.out.println("You are too low");
+                        Guess = Guess + 1;
+                    }
+                }while (Guess != secretNumber);
             }
-        }while (Guess != secretNumber);
-    }
-    public static void second(String[] args)
-    {
-        Scanner keyboard = new Scanner(System.in);
-        System.out.println("I Can Guess Your Number");
-        String result = keyboard.nextLine();
+          public static void second(String[] args)
+            {
+                Scanner keyboard = new Scanner(System.in);
+                System.out.println("I Can Guess Your Number That Is Between 0 and 100");
+                String result = keyboard.nextLine();
+                System.out.println("Is it 50?")
+                    if (result == correct)
+                    {
+                        System.out.println("That Was Too Easy Nerd")
+                    }
+                    if (result == greater)
+                    {
 
+                    }
+            }
+        }
     }
 }
